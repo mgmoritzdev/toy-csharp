4a38d1c49ff2c84c51dc8c0c283c1200d2ce271c
Marcos Moritz <moritz@portaltelemedicina.com.br>
2021-05-16 06:54:25 -0300

    Change the name of output file to toy

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-/usr/bin/csc /out:csharp *.cs && chmod 755 csharp
-# /usr/bin/csc /reference:System.Net.Http.dll /reference:Newtonsoft.Json.dll /out:csharp *.cs && chmod 755 csharp
+/usr/bin/csc /out:toy *.cs && chmod 755 toy
+# /usr/bin/csc /reference:System.Net.Http.dll /reference:Newtonsoft.Json.dll /out:toy *.cs && chmod 755 toy
